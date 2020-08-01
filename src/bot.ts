aec6e4b7a5710287de8c5e32b9c949d7f643bf89
holyshared <holy.shared.design@gmail.com>
2020-08-02 00:43:27 +0900

    add bot sources

---
+++ src/bot.ts
@@ -0,0 +1,28 @@
+import { TeamsActivityHandler, TurnContext, MessageFactory, ChannelAccount } from 'botbuilder';
+
+export class Bot extends TeamsActivityHandler {
+  constructor() {
+    super();
+    this.onConversationUpdate(this._handleConversationUpdate);
+    this.onMembersAdded(this._handleMembersAdded);
+  }
+
+  async _handleConversationUpdate(_context: TurnContext, next: () => Promise<void>): Promise<void> {
+    await next();
+  }
+
+  async _handleMembersAdded(context: TurnContext, next: () => Promise<void>): Promise<void> {
+    const membersAdded = context.activity.membersAdded;
+    const addedMemners = membersAdded.filter((addedMember: ChannelAccount) => addedMember.id !== context.activity.recipient.id);
+    const welcomeMessages = addedMemners.map(
+      async (addedMemner: ChannelAccount) => this.sendWelcomeMessage(addedMemner, context)
+    );
+    await Promise.all(welcomeMessages);
+    await next();
+  }
+
+  async sendWelcomeMessage(member: ChannelAccount, context: TurnContext): Promise<void> {
+    const message = MessageFactory.text(`Welcome ${member.name}`);
+    await context.sendActivity(message);
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import Koa from 'koa';
+import Router from '@koa/router';
+import { RouterContext } from 'koa-router';
+import { BotFrameworkAdapter, TurnContext } from 'botbuilder';
+import { Bot } from './bot';
+
+const app = new Koa();
+const router = new Router();
+
+const adaptor = new BotFrameworkAdapter({
+  appId: process.env.BOT_APP_CLIENT_ID,
+  appPassword: process.env.BOT_APP_CLIENT_PASSWORD
+});
+
+const bot = new Bot();
+
+const createExpressLikeResponse = (ctx: RouterContext<{}, {}>) => ({
+  status(code: number): void {
+    ctx.status = code;
+  },
+  send(body: string): void {
+    ctx.body = body;
+  },
+  end(): void {
+  }
+});
+
+router.post('/api/messages', async (ctx: RouterContext<{}, {}>) => {
+  const { request: req } = ctx;
+  const res = createExpressLikeResponse(ctx);
+
+  await adaptor.processActivity(req, res, async (turnContext: TurnContext): Promise<void> => {
+    await bot.run(turnContext);
+  });
+});
+
+app.use(router.routes())
+  .use(router.allowedMethods())
+  .listen(3000);
